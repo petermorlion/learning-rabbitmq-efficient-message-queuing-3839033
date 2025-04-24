f8a0de83f52051128f0e7e7916fd641dc2b59fcf
Peter Morlion <peter.morlion@gmail.com>
2025-04-24 18:23:34 +0200

    Add ExploreCalifornia pages

---
+++ WebApp/Controllers/BookingController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreCalifornia.WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Book")]
+        public async Task<IActionResult> Book()
+        {
+            var tourname = Request.Form["tourname"];
+            var name = Request.Form["name"];
+            var email= Request.Form["email"];
+            var needsTransport = Request.Form["transport"] == "on";
+
+            // Send messages here...
+
+            return Redirect($"/BookingConfirmed?tourname={tourname}&name={name}&email={email}");
+        }
+
+        [HttpPost]
+        [Route("Cancel")]
+        public async Task<IActionResult> Cancel()
+        {
+            var tourname = Request.Form["tourname"];
+            var name = Request.Form["name"];
+            var email = Request.Form["email"];
+            var cancelReason = Request.Form["reason"];
+
+            // Send cancel message here
+
+            return Redirect($"/BookingCanceled?tourname={tourname}&name={name}");
+        }
+    }
+}
--- WebApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ExploreCalifornia.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
