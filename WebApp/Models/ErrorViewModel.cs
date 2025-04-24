4abbe78a5ba163d0c2acf88d7c1e34bdc6e20ad0
Peter Morlion <peter.morlion@gmail.com>
2025-04-24 17:46:24 +0200

    Add new MVC project

---
+++ WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExploreCalifornia.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
