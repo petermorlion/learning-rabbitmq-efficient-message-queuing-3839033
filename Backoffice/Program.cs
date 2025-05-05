639815070a499a9275739c98b7b264847d63a2f4
Peter Morlion <peter.morlion@gmail.com>
2025-05-05 16:00:57 +0200

    Move to topic exchange and subscribe backoffice to the messages

--- Backoffice/Program.cs
+++ Backoffice/Program.cs
@@ -1,11 +1,24 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 var factory = new ConnectionFactory();
 factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
 var connection = await factory.CreateConnectionAsync();
 var channel = await connection.CreateChannelAsync();
 
-// declare resources here, handle consumed events, etc.
+await channel.QueueDeclareAsync("backOfficeQueue", true, false, false);
+await channel.QueueBindAsync("backOfficeQueue", "webappExchange", "tour.*");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (sender, eventArgs) =>
+{
+    var msg = System.Text.Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+    Console.WriteLine($"{eventArgs.RoutingKey} : ${msg}");
+};
+
+await channel.BasicConsumeAsync("backOfficeQueue", true, consumer);
+
+Console.ReadLine();
 
 await channel.CloseAsync();
 await connection.CloseAsync();
