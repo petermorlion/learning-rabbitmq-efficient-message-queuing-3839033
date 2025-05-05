c6f6be0626fbe2ff9e351e3a0e10fd40cdbd8cbc
Peter Morlion <peter.morlion@gmail.com>
2025-05-05 15:33:51 +0200

    Add connection code

--- Backoffice/Program.cs
+++ Backoffice/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+var connection = await factory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+
+// declare resources here, handle consumed events, etc.
+
+await channel.CloseAsync();
+await connection.CloseAsync();
+
